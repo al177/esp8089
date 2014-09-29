8fb48c4db1a306405ce444f996c593c5d6caaca7
Rockchip <service@rock-chips.com>
2014-09-29 17:00:22 +0800

    wifi: update the esp8089 driver V1.7.

--- sdio_stub.c
+++ sdio_stub.c
@@ -7,7 +7,7 @@
 #include <mach/gpio.h>
 #include <mach/iomux.h>
 
-#define ESP8089_DRV_VERSION "2.20"
+#define ESP8089_DRV_VERSION "1.03"
 
 extern int rk29sdk_wifi_power(int on);
 extern int rk29sdk_wifi_set_carddetect(int val);
@@ -32,15 +32,15 @@ void sif_platform_reset_target(void)
 	if(sif_get_bt_config() == 1 && sif_get_retry_config() == 0){
 		if(sif_get_rst_config() == 1){
 			//TODO, use gpio to reset target
-		}// else {
-		//	iomux_set(GPIO3_D2);
-		//	gpio_request(RK30_PIN3_PD2, "esp8089-sdio-wifi");
+		} else {
+			iomux_set(GPIO3_D2);
+			gpio_request(RK30_PIN3_PD2, "esp8089-sdio-wifi");
 
-		//	mdelay(100);
-		//	gpio_direction_output(RK30_PIN3_PD2, 0);
-		//	mdelay(50);
-		//	gpio_direction_output(RK30_PIN3_PD2, 1);
-	//	}
+			mdelay(100);
+			gpio_direction_output(RK30_PIN3_PD2, 0);
+			mdelay(50);
+			gpio_direction_output(RK30_PIN3_PD2, 1);
+		}
 	} else {
 		//rk29sdk_wifi_set_carddetect(0);
         	rk29sdk_wifi_power(0);
@@ -94,10 +94,16 @@ void sif_platform_ack_interrupt(struct esp_pub *epub)
         struct esp_sdio_ctrl *sctrl = NULL;
         struct sdio_func *func = NULL;
 
-        ASSERT(epub != NULL);
+	if (epub == NULL) {
+        	ESSERT(epub != NULL);
+		return;
+	}
         sctrl = (struct esp_sdio_ctrl *)epub->sif;
         func = sctrl->func;
-        ASSERT(func != NULL);
+	if (func == NULL) {
+        	ESSERT(func != NULL);
+		return;
+	}
 
         sdmmc_ack_interrupt(func->card->host);
 }
